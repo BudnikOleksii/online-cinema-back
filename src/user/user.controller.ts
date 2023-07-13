27f7425f3f988f379fb6fbe90286c70cfd5562e2
Oleksii Budnik <budnikoleksii@gmail.com>
2023-07-13 11:21:36 +0300

    added users service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,70 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Put,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 
 import { UserService } from './user.service';
 import { Auth } from '../auth/decorators/auth.decorator';
 import { User } from './decorators/user.decorator';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { IdValidationPipe } from '../pipes/id.validation.pipe';
 
 @Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly UserService: UserService) {}
 
   @Get('profile')
   @Auth()
   async getProfile(@User('_id') _id: string) {
-    return this.userService.byId(_id);
+    return this.UserService.byId(_id);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Put('profile')
+  @HttpCode(200)
+  @Auth()
+  async updateProfile(@User('_id') _id: string, @Body() dto: UpdateUserDto) {
+    return this.UserService.updateProfile(_id, dto);
+  }
+
+  @Get('count')
+  @Auth('admin')
+  async getCountUser() {
+    return this.UserService.getCount();
+  }
+
+  @Get()
+  @Auth('admin')
+  async getUsers(@Query('searchTerm') searchTerm?: string) {
+    return this.UserService.getAll(searchTerm);
+  }
+
+  @Get(':id')
+  @Auth('admin')
+  async getUser(@Param('id', IdValidationPipe) _id: string) {
+    return this.UserService.byId(_id);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Put(':id')
+  @HttpCode(200)
+  @Auth('admin')
+  async updateUser(@Param('id', IdValidationPipe) _id: string, @Body() dto: UpdateUserDto) {
+    return this.UserService.updateProfile(_id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(200)
+  @Auth('admin')
+  async deleteUser(@Param('id', IdValidationPipe) _id: string) {
+    return this.UserService.delete(_id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,11 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from 'nestjs-typegoose';
 import { ModelType } from '@typegoose/typegoose/lib/types';
 
 import { UserModel } from './user.model';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { genSalt, hash } from 'bcryptjs';
+import { use } from 'passport';
 
 @Injectable()
 export class UserService {
@@ -16,5 +19,53 @@ export class UserService {
     }
 
     return user;
+  }
+
+  async updateProfile(_id: string, dto: UpdateUserDto) {
+    const user = await this.byId(_id);
+    const isSameUser = await this.UserModel.findOne({ email: dto.email });
+
+    if (isSameUser && String(_id) !== String(isSameUser._id)) {
+      throw new BadRequestException('Email is already in use');
+    }
+
+    if (dto.password) {
+      const salt = await genSalt(10);
+      user.password = await hash(dto.password, salt);
+    }
+
+    user.email = dto.email;
+
+    if (dto.isAdmin || dto.isAdmin === false) {
+      user.isAdmin = dto.isAdmin;
+    }
+
+    return user.save();
+  }
+
+  getCount() {
+    return this.UserModel.countDocuments();
+  }
+
+  getAll(searchTerm?: string) {
+    let options = {};
+
+    if (searchTerm) {
+      options = {
+        $or: [
+          {
+            email: new RegExp(searchTerm, 'i'),
+          },
+        ],
+      };
+    }
+
+    return this.UserModel.find(options)
+      .select('-password -updatedAt -__v')
+      .sort({ createdAt: 'desc' });
+  }
+
+  delete(id: string) {
+    return this.UserModel.findByIdAndDelete(id);
   }
 }
